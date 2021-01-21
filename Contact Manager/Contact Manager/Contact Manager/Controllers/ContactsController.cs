2d31f13b7d8b15ba61c6c2290962aaed32b08ab7
Oleh Sytnyk <olexmessi@gmail.com>
2021-01-21 22:58:26 +0200

    Create fi;ter + cvs upload

--- Contact Manager/Contact Manager/Contact Manager/Controllers/ContactsController.cs
+++ Contact Manager/Contact Manager/Contact Manager/Controllers/ContactsController.cs
@@ -4,6 +4,14 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ContactManager.Data;
 using ContactManager.Models;
+using System;
+using System.Data;
+using System.IO;
+using System.Text.RegularExpressions;
+using Microsoft.Data.SqlClient;
+using System.Configuration;
+using System.Web;
+using Grpc.Core;
 
 namespace ContactManager.Controllers
 {
@@ -16,11 +24,141 @@ namespace ContactManager.Controllers
             _context = context;
         }
 
-        // GET: Contacts
-        public async Task<IActionResult> Index()
+        /*[HttpPost]
+        public ActionResult Index(HttpPostedFileBase FileUpload)
         {
-            return View(await _context.Contacts.ToListAsync());
+
+            DataTable dt = new DataTable();
+
+
+            if (FileUpload.ContentLength > 0)
+            {
+
+                string fileName = Path.GetFileName(FileUpload.FileName);
+                string path = Path.Combine(Server.MapPath("~/uploads"),fileName);
+
+
+                try
+                {
+                    FileUpload.SaveAs(path);
+
+                    dt = ProcessCSV(path);
+
+
+                    ViewData["Feedback"] = ProcessBulkCopy(dt);
+                }
+                catch (Exception ex)
+                {
+
+                    ViewData["Feedback"] = ex.Message;
+                }
+            }
+            else
+            {
+
+                ViewData["Feedback"] = "Please select a file";
+            }
+
+
+            dt.Dispose();
+
+            return View("Index", ViewData["Feedback"]);
         }
+
+        private static DataTable ProcessCSV(string fileName)
+        {
+
+            string Feedback = string.Empty;
+            string line = string.Empty;
+            string[] strArray;
+            DataTable dt = new DataTable();
+            DataRow row;
+
+
+            Regex r = new Regex(",(?=(?:[^\"]*\"[^\"]*\")*(?![^\"]*\"))");
+
+
+            StreamReader sr = new StreamReader(fileName);
+
+
+            line = sr.ReadLine();
+            strArray = r.Split(line);
+
+
+            Array.ForEach(strArray, s => dt.Columns.Add(new DataColumn()));
+
+
+
+            while ((line = sr.ReadLine()) != null)
+            {
+                row = dt.NewRow();
+
+
+                row.ItemArray = r.Split(line);
+                dt.Rows.Add(row);
+            }
+
+
+            sr.Dispose();
+
+
+            return dt;
+
+
+        }
+
+
+        private static String ProcessBulkCopy(DataTable dt)
+        {
+            string Feedback = string.Empty;
+            string connString = ConfigurationManager.ConnectionStrings["ContactManagerContext"].ConnectionString;
+
+
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+
+                using (var copy = new SqlBulkCopy(conn))
+                {
+
+
+                    conn.Open();
+
+
+                    copy.DestinationTableName = "ImportDetails";
+                    copy.BatchSize = dt.Rows.Count;
+                    try
+                    {
+
+                        copy.WriteToServer(dt);
+                        Feedback = "Upload complete";
+                    }
+                    catch (Exception ex)
+                    {
+                        Feedback = ex.Message;
+                    }
+                }
+            }
+
+            return Feedback;
+        }*/
+
+        // GET: Contacts
+        public async Task<IActionResult> Index(string searchString)
+        {
+
+            var title = from m in _context.Contacts
+                        select m;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                title = title.Where(s => s.Name.Contains(searchString)
+                                    || s.Phone.Contains(searchString)
+                                    || s.Salary.Equals(searchString));
+            }
+
+            return View(await title.ToListAsync());
+        }
+
 
         // GET: Contacts/Details/5
         public async Task<IActionResult> Details(int? id)
